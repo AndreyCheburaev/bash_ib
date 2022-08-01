0f8f37c69e2b09c3e725a9fd9d914a18c72952ee
fedor <cha@get8.ru>
2022-08-01 23:23:25 +0300

    Добавляет скрипт проверки портов серверов

--- ports_scan/scan.sh
+++ ports_scan/scan.sh
@@ -1,36 +1,26 @@
 #!/bin/bash
+#
+# Example ./scan.sh <list_ip> <out_log>
+
+IP_LIST=${1:-'ip_list.txt'}
+printf -v TODAY 'scan_%(%F)T.txt' -1
+OUTFILE=${2:-$TODAY}
 
 function scan()
 {
 	host=$1
 	printf '%s' "$host"
 	
-	for ((port=1;port<1024;port++))
+	for ((port=1;port<24;port++))
 	do
 		echo >/dev/null 2>&1 < /dev/tcp/${host}/${port}
-		#printf "$?"
+
 		if (($? == 0))  ; then printf ' %d' "$port"  ; fi
 	done
 	echo
 }
 
-#printf -v TODAY 'scan_%(%F)T' -1
-printf -v TODAY 'scan_res' -1
-
-OUTFILE=${1:-$TODAY}
-
-#echo "$HOSTNAME"
-#		echo >/dev/null 2>&1 < /dev/tcp/192.168.1.79/443
-
-echo start
-
-HOSTNAMES=78.155.219.50
-HOSTNAMES=46.101.86.163
-
-#echo "$(read HOSTNAME)"
-
-#while read $HOSTNAMES
-#do
-	echo "$HOSTNAMES"
-	scan $HOSTNAMES > $OUTFILE
-#done #> $OUTFILE
+while read ip
+do
+    scan ${ip}
+done < ${IP_LIST} > ${OUTFILE}
